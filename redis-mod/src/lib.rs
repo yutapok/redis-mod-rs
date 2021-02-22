6d0145decef95af1a956dc482822c63fb062a55b
yutapok <yutautsugi@outlook.com>
2021-02-22 21:15:37 +0900

    modify: change name

--- redis-mod/src/lib.rs
+++ redis-mod/src/lib.rs
@@ -16,13 +16,21 @@ use libc::c_int;
 const MODULE_NAME: &str = "rmod";
 const MODULE_VERSION: c_int = 1;
 
-pub struct InitWithRedisAlloc {
-    pub module_name: &'static str,
-    pub module_version: c_int
+pub struct RedisModuleInitializer {
+    module_name: &'static str,
+    module_version: c_int
 }
 
-impl InitWithRedisAlloc {
-    pub fn init(&self,
+impl RedisModuleInitializer {
+    pub fn new(
+        mod_name: &'static str,
+        mod_ver: c_int
+    ) -> Self {
+      module_name: mod_name,
+      module_version: mod_ver
+    }
+
+    pub fn do(&self,
         ctx: *mut raw::RedisModuleCtx,
         argv: *mut *mut raw::RedisModuleString,
         argc: c_int
