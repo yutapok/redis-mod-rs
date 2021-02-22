a58c419c52a0bce22ef25ffdd225376ef851eca8
yutapok <yutautsugi@outlook.com>
2021-02-22 20:58:52 +0900

    modify: ini

--- redis-mod/src/lib.rs
+++ redis-mod/src/lib.rs
@@ -7,7 +7,37 @@ extern crate time;
 pub mod macros;
 
 pub mod redis;
-pub use crate::redis::{raw, Command, RedisAlloc};
+pub use crate::redis::{raw, Command};
 pub mod error;
 pub use crate::error::RModError;
 
+use libc::c_int;
+
+const MODULE_NAME: &str = "rmod";
+const MODULE_VERSION: c_int = 1;
+
+struct InitWithRedisAlloc {
+    module_name: &'static str,
+    module_version: c_int
+}
+
+impl InitWithRedisAlloc {
+    fn init(&self,
+        ctx: *mut raw::RedisModuleCtx,
+        argv: *mut *mut raw::RedisModuleString,
+        argc: c_int
+    ) -> raw::Status {
+        if raw::init(
+            ctx,
+            format!("{}\0", self.module_name).as_ptr(),
+            self.module_version,
+            raw::REDISMODULE_APIVER_1,
+        ) == raw::Status::Err
+        {
+            return raw::Status::Err;
+        }
+
+        return raw::Status::Ok;
+
+    }
+}
